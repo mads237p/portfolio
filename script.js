dedb7932895313cabec86dd9f9771d700edc484e
Oliver Kesi <oliverkesi@laptop-vml8vi2q.kea.local>
2019-06-13 15:38:33 +0200

    Alle overskrifter positioneret rigtigt. Nav virker. Menu virker.
    Mangler animation.

--- script.js
+++ script.js
@@ -4,28 +4,32 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("sidenVises");
 
-document.querySelector("#start").classList.add("start_ani");
+    //document.querySelector("#start").classList.add("start_ani");
 
-document.querySelector("#pil_right").addEventListener("click", slide);
+    //    document.querySelector("#pil_right").addEventListener("click", slide);
 
-document.querySelector("#menu_knap").addEventListener("click", menu);
-
-}
-
-function slide () {
-    console.log("slide");
-
-//    document.querySelector("#forside").classList.add("slide");
+    document.querySelector(".menuknap").addEventListener("click", toggleMenu);
 
 }
 
-function menu() {
-    console.log("menu");
+//function slide() {
+//    console.log("slide");
+//
+//    //    document.querySelector("#forside").classList.add("slide");
+//
+//}
 
-  document.querySelector("#menu").classList.remove("hidden");
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
 
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.getElementsByClassName("menuknap").src = "assets/menu_dots";
+    } else {
+        document.getElementsByClassName("menuknap").src = "assets/No.svg";
+        document.getElementsByClassName("cv").classlist = "hidden";
+        document.getElementsByClassName("forside").classlist = "hidden";
+    }
 }
-
-
-
-
