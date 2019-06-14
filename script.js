bced59956727bfc67ff77ba76891e9ef571ed767
Oliver Kesi <oliverkesi@Olivers-MacBook-Pro-2.local>
2019-06-14 15:48:47 +0200

    Slides virker og animationer på forside virker. Menu virker

--- script.js
+++ script.js
@@ -1,37 +1,66 @@
 window.addEventListener("load", sidenVises);
 
 
+
+var elmnt1 = document.getElementById("slide-1");
+var elmnt2 = document.getElementById("slide-2");
+var elmnt3 = document.getElementById("slide-3");
+var elmnt4 = document.getElementById("slide-4");
+var page;
+
+document.querySelector("#start").classList.add("startAni");
+document.querySelector("#forside").classList.add("fadeIn");
+
 function sidenVises() {
     console.log("sidenVises");
 
-    //document.querySelector("#start").classList.add("start_ani");
+    let frontPage = document.getElementById("start");
+
+    if(frontPage == true) {
+
+        document.querySelector("#start").classList.add("startAni");
+
+        } else {
+            startSide();
+        }
 
 
-    document.querySelector(".pil_right").addEventListener("click", slide);
-    document.querySelector(".pil_left").addEventListener("click", slide);
+//document.querySelector("#start").classList.add("startAni");
+//
+//
+//  document.querySelector("#start").addEventListener("animationend", startSide);
 
-    document.querySelector(".menuknap").addEventListener("click", toggleMenu);
 
 }
 
-function slide() {
-    console.log("slide");
 
-    document.querySelector("#forside").classList.add("page_slide_right");
+function startSide() {
+    console.log("startSide");
+
+
+       document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+//      document.querySelector(".pil_right").addEventListener("click", scrollTo);
+//    document.querySelector(".pil_left").addEventListener("click", scrollTo);
+
+}
+
+
+function scrollTo() {
+    console.log("scroll");
+
+     document.querySelector(".pil_right").removeEventListener("click", scrollTo);
+    document.querySelector(".pil_left").removeEventListener("click", scrollTo);
+
+    document.getElementsById("front").classList.add("page_slide_right");
 
 }
 
 function toggleMenu() {
     console.log("toggleMenu");
+
     document.querySelector("#menu").classList.toggle("hidden");
 
     let erSkjult = document.querySelector("#menu").classList.contains("hidden");
 
-    if (erSkjult == true) {
-        document.getElementsByClassName("menuknap").src = "assets/menu_dots";
-    } else {
-        document.getElementsByClassName("menuknap").src = "assets/No.svg";
-        document.getElementsByClassName("cv").classlist = "hidden";
-        //        document.getElementsByClassName("forside").classlist = "hidden";
-    }
 }
